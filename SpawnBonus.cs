6e90e30a269d38755b4832d28ea3ba25af68c13b
Dellfi <jallson1771@gmail.com>
2018-06-13 01:00:41 +0300

    Add SpawnBonus

---
+++ SpawnBonus.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnBonus : MonoBehaviour
+{
+	public GameObject[] Bonus = new GameObject[3];
+
+	private void Awake()
+	{
+		GameObject tempBonus = Bonus [2];
+		Instantiate (tempBonus);
+	}
+}
