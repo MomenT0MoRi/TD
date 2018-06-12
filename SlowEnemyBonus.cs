c71884f64019cacbec7214f05c6244c8d72797b4
Dellfi <jallson1771@gmail.com>
2018-06-13 01:00:08 +0300

    Add slowEnemyBonus

---
+++ SlowEnemyBonus.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SlowEnemyBonus : MonoBehaviour
+{
+	public GameObject Enemy;
+
+	void OnMouseDown()
+	{
+		Enemy.GetComponent<Enemy> ().BonusSpeedSlow (2.0f);
+	}
+}
